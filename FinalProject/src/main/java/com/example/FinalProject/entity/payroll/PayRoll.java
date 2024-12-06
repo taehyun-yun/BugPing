de4fc4db5ce7ff22600559111cd2db74c1fcfb4e
junho <junho9661@gmail.com>
2024-12-06 18:24:24 +0900

    241206 엔티티 전부 완성!

---
+++ FinalProject/src/main/java/com/example/FinalProject/entity/payroll/PayRoll.java
@@ -0,0 +1,27 @@
+package com.example.FinalProject.entity.payroll;
+
+import com.example.FinalProject.entity.working.Work;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class PayRoll {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer payRollId;
+    private LocalDate paymentDate;
+    private int monthPayment;
+    private int incentive;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "workId")
+    private Work work;
+
+}
+
