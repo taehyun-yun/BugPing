50e285a18845b3ce79b40bd80db55ab000261353
REVE <aiwork4242@gmail.com>
2024-12-06 11:32:06 +0900

    계약 레파지토리 머지했을때 안보여서 다시 #8

--- FinalProject/src/main/java/com/example/FinalProject/repository/employment/ContractRepository.java
+++ FinalProject/src/main/java/com/example/FinalProject/repository/employment/ContractRepository.java
@@ -4,4 +4,5 @@ import com.example.FinalProject.entity.employment.Contract;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ContractRepository extends JpaRepository<Contract, Integer> {
+
 }
