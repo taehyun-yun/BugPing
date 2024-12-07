1ecac4e12205638e538b3727186e130eceed8cf2
junho <junho9661@gmail.com>
2024-12-07 17:43:11 +0900

    file 엔티티 삽입

---
+++ FinalProject/src/main/java/com/example/FinalProject/entity/file/File.java
@@ -0,0 +1,22 @@
+package com.example.FinalProject.entity.file;
+
+import com.example.FinalProject.entity.notice.Notice;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class File {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer FileId;
+    @ManyToOne
+    @JoinColumn(name = "Notice_id")
+    private Notice notice;
+    private String filePath;
+    private String fileType;
+}
