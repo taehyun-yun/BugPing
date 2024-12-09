8383f4b83a3b86b771acaf2ee68ce30972704b9f
junho <junho9661@gmail.com>
2024-12-09 19:08:14 +0900

    엔티티  1. change -> workChange 로 변경  2. contract에 companyId 삭제  3. Attendance minute으로 변경, 오타 수정  4. noticecontroller 일단 주석처리하고 올립니다.

--- FinalProject/src/main/java/com/example/FinalProject/entity/attendance/Attendance.java
+++ FinalProject/src/main/java/com/example/FinalProject/entity/attendance/Attendance.java
@@ -1,6 +1,8 @@
 package com.example.FinalProject.entity.attendance;
 
 import com.example.FinalProject.entity.contract.Contract;
+import com.example.FinalProject.entity.schedule.Schedule;
+import com.example.FinalProject.entity.schedule.WorkChange;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -17,9 +19,20 @@ public class Attendance {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer attendanceId;
-    @ManyToOne
-    @JoinColumn(name = "contract_id")
-    private Contract contract;
+    //1안
+    @ManyToOne//(fetch = FetchType.LAZY)
+    @JoinColumn(name = "schedule_id")//, nullable = false, insertable = false, updatable = false
+    private Schedule schedule;
+
+    //1안
+    @ManyToOne//(fetch = FetchType.LAZY)
+    @JoinColumn(name = "change_id")//, nullable = false, insertable = false, updatable = false
+    private WorkChange workChange;
+
+//    2안
+//    @ManyToOne
+//    @JoinColumn(name = "contract_id")
+//    private Contract contract;
     private LocalDateTime actualStart;
     private LocalDateTime actualEnd;
     private String commuteStatus;
