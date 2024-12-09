ff40a5b63c8103ea785f482b51df66dc75e4c155
REVE <aiwork4242@gmail.com>
2024-12-09 14:51:11 +0900

    1209 계약&스케쥴 컨트롤러 리파지토리 임시 저장 #10

--- FinalProject/src/main/java/com/example/FinalProject/repository/employment/ScheduleRepository.java
+++ FinalProject/src/main/java/com/example/FinalProject/repository/employment/ScheduleRepository.java
@@ -4,6 +4,9 @@ package com.example.FinalProject.repository.employment;
 import com.example.FinalProject.entity.employment.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ScheduleRepository extends JpaRepository<Schedule, Integer> {
+import java.util.List;
 
+public interface ScheduleRepository extends JpaRepository<Schedule, Integer> {
+    // 특정 Contract ID로 스케줄을 조회하는 메서드
+    List<Schedule> findByContractContractId(Integer contractId);
 }
