2576cab687455e2141e6b2990b60461f219bd2b7
REVE <aiwork4242@gmail.com>
2024-12-09 12:25:30 +0900

    1209 Attendance FK 수정 1안으로 진행 예정 #7

--- FinalProject/src/main/java/com/example/FinalProject/entity/attendance/Attendance.java
+++ FinalProject/src/main/java/com/example/FinalProject/entity/attendance/Attendance.java
@@ -1,6 +1,9 @@
 package com.example.FinalProject.entity.attendance;
 
 import com.example.FinalProject.entity.employment.Contract;
+import com.example.FinalProject.entity.employment.Schedule;
+import com.example.FinalProject.entity.employment.WorkChange;
+import com.example.FinalProject.entity.work.Work;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -18,9 +21,20 @@ public class Attendance {
     //@Column(name = "attendanceId", nullable = false, updatable = false)
     private Integer attendanceId;
 
+    //1안
     @ManyToOne//(fetch = FetchType.LAZY)
-    @JoinColumn(name = "contract_id")//, nullable = false, insertable = false, updatable = false
-    private Contract contract;
+    @JoinColumn(name = "schedule_id")//, nullable = false, insertable = false, updatable = false
+    private Schedule schedule;
+
+    //1안
+    @ManyToOne//(fetch = FetchType.LAZY)
+    @JoinColumn(name = "change_id")//, nullable = false, insertable = false, updatable = false
+    private WorkChange workChange;
+
+//    //2안
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "work_id")//, nullable = false, insertable = false, updatable = false
+//    private Work work;
 
     private LocalTime actualStart;
     private LocalTime actualEnd;
