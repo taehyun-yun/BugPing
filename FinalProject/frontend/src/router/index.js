5def9da890595b877726de0cb3d5d936d1539a2a
junho <junho9661@gmail.com>
2024-12-10 11:38:50 +0900

    employer, employee 페이지 추가. 회원가입 진행중, 주소 api 넣는 중

--- FinalProject/frontend/src/router/index.js
+++ FinalProject/frontend/src/router/index.js
@@ -13,6 +13,9 @@ import SignUpView from '@/views/auth/SignUpView.vue';
 import SU1 from '@/components/auth/SU1.vue';
 import SU2 from '@/components/auth/SU2.vue';
 import SU3 from '@/components/auth/SU3.vue';
+import Employer from '@/views/employer.vue';
+import Employee from '@/views/Employee.vue';
+
 
 const router = createRouter({
 history: createWebHistory(import.meta.env.BASE_URL),
@@ -60,13 +63,23 @@ routes: [
         meta : { header : true, sidebar : true, requiresAuth: false, title: '계약',},
     },
     { path: '/login', name: 'login', component: LoginView, meta : {title: '로그인'} },
-    { path: '/signup', name : 'signup', component : SignUpView,
+    { path: '/', name : 'signup', component : SignUpView, meta : {title: '회원가입'},
       children : [
-        {path: 'su1', name : 'su1', component : SU1},
-        {path: 'su2', name : 'su2', component : SU2},
-        {path: 'su3', name : 'su3', component : SU3},
+        {path: 'su1', name : 'su1', component : SU1,},
+        {path: 'su2', name : 'su2', component : SU2,},
+        {path: 'su3', name : 'su3', component : SU3,},
       ],
-       meta : {title: '회원가입'}}
+       },
+       { path : '/employer', name : 'employer', component : Employer, meta : {header : true, sidebar : true, requiresAuth: false, roles : ["employer"],
+        children : [
+          //이 안에 넣으시면 됩니다.
+        ]
+       }},
+       { path : '/employee', name : 'employee', component : Employee, meta : {header : true, sidebar : true, requiresAuth: false, roles : ["employee"],
+        children : [
+          //이 안에 넣으시면 됩니다.
+        ]
+       }}
     // ↓↓예시↓↓ 인증이 필요한 페이지는 뒤에 meta: {requiresAuth: true } 넣어주면 됩니다. ↓↓예시↓↓
     //{ path: '/protected', name: 'Protected', component: ProtectedPage, meta: { header : true, sidebar : true, requiresAuth: true, roles: ['employer'], } }
     //{ path: '/unprotected', name: 'UnProtected', component: UnProtectedPage, }
