e643c6e611cc3f9071ea2931f9684fa41fb79d99
REVE <aiwork4242@gmail.com>
2024-12-11 19:06:16 +0900

    1210 repository 패치조인 (참고사항) #10

--- FinalProject/src/main/java/com/example/FinalProject/repository/attendance/AttendanceRepository.java
+++ FinalProject/src/main/java/com/example/FinalProject/repository/attendance/AttendanceRepository.java
@@ -47,4 +47,16 @@ public interface AttendanceRepository  extends JpaRepository<Attendance, Integer
             "JOIN FETCH c.work w " +
             "JOIN FETCH w.user u")
     List<Attendance> findAllAttendancesWithWorkChangeScheduleContractWorkAndUser();
+
+
+
+    //출퇴근-체인지 엔티티 연결 끊고 스케쥴. 변경사항으로 타고 들어가게 하는 방법도 고려해볼 것
+    @Query("SELECT DISTINCT a FROM Attendance a " +
+            "JOIN FETCH a.schedule s " +
+            //"JOIN FETCH a.workChange wc " + // WorkChange와 패치 조인은 제외
+            "JOIN FETCH s.contract c " +
+            "JOIN FETCH c.work w " +
+            "JOIN FETCH w.user u " +
+            "WHERE a.id = :attendanceId")
+    Attendance findAttendanceWithAll(@Param("attendanceId") Long attendanceId);
 }
