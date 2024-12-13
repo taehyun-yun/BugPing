d5954fd47f7cc2d5f0a249441a9276444797fb4e
REVE <aiwork4242@gmail.com>
2024-12-13 19:46:43 +0900

    1213 스케쥴 모달에 요일까지 연동 완료 #11

--- FinalProject/frontend/src/components/employment/ScheduleModal.vue
+++ FinalProject/frontend/src/components/employment/ScheduleModal.vue
@@ -205,17 +205,26 @@ const handleConfirm = () => {
 };
 
 
-
-
 const weekdays = [
-  { label: '월', value: 'MON' },
-  { label: '화', value: 'TUE' },
-  { label: '수', value: 'WED' },
-  { label: '목', value: 'THU' },
-  { label: '금', value: 'FRI' },
-  { label: '토', value: 'SAT' },
-  { label: '일', value: 'SUN' }
-]
+  { label: '월', value: 1 },
+  { label: '화', value: 2 },
+  { label: '수', value: 3 },
+  { label: '목', value: 4 },
+  { label: '금', value: 5 },
+  { label: '토', value: 6 },
+  { label: '일', value: 7 },
+];
+
+
+// const weekdays = [
+//   { label: '월', value: 'MON' },
+//   { label: '화', value: 'TUE' },
+//   { label: '수', value: 'WED' },
+//   { label: '목', value: 'THU' },
+//   { label: '금', value: 'FRI' },
+//   { label: '토', value: 'SAT' },
+//   { label: '일', value: 'SUN' }
+// ]
 
 
 </script>
