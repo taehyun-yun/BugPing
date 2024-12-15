58253bbf5455ae368c086da4bf3fcd0f9674dd50
junho <junho9661@gmail.com>
2024-12-15 12:05:07 +0900

    헤더 사이드바 전부 수정 indexjs 전부 수정 로그인, 회원가입시 비밀번호 암호 보기 기능 넣음 근무자 등록 페이지 제작  - build gradle에 nanoId 추가  - company에 가입 코드 추가.  - 근무자 회사 가입 방식 구현

---
+++ FinalProject/frontend/src/views/Main.vue
@@ -0,0 +1,41 @@
+<template>
+        <MainHeader @show-sidebar="showSidebar($event)"></MainHeader>
+        <MainSidebar v-show="sidebar"></MainSidebar>
+    <div class="total">
+        <div class="routerview-container" :class="{ 'exist-sidebar' : sidebar }">
+            <RouterView :key="$route.fullPath"></RouterView>
+        </div>
+    </div>
+</template>
+<script setup>
+import MainHeader from '@/components/MainHeader.vue';
+import MainSidebar from '@/components/MainSidebar.vue';
+import { ref } from 'vue';
+import { useRoute } from 'vue-router';
+const route = useRoute();
+const mainheader = ref(route?.meta?.header === true);
+const sidebar = ref(route?.meta?.sidebar === true);
+// alert("사이드바를 표시하나요? "+sidebar.value);
+const showSidebar = (menucontroll) =>{
+    sidebar.value = menucontroll;
+}
+</script>
+<style scoped>
+    .total {
+        width: 100vw;
+        display: flex;
+        justify-content: center;
+        margin-top: 70px;
+    }
+    .routerview-container{
+        width: 100%;
+        display: inline-block;
+        margin : 0 auto;
+        white-space: nowrap;
+    }
+    .exist-sidebar{
+        min-width: 1000px;
+        max-width: 1400px;
+        padding-left : 200px;
+    }
+</style>
---
+++ FinalProject/frontend/src/views/workplace/workplace.vue
@@ -0,0 +1,178 @@
+<template>
+<div class="wrap-workplace">
+    <div class="search-bar-container">
+        <div class="search-bar">
+            <input class="input-field" v-model="inputCompanyCode">
+            <img src="/src/assets/enroll/magnifying-glass-solid.svg" class="search-button" @click="getInfo">
+        </div>
+    </div>
+    <div class="companyInfo">
+
+    </div>
+    <Teleport to="body">
+        <div class="modal-overlay" v-show="showChangeModal" @click.self="closeModal">
+            <div class="modal">
+                <div class="title">
+                    <img class="icon" src="/src/assets/enroll/briefcase-solid.svg"> {{ companyInfo.cname }}
+                </div>
+                <div class="content">
+                    <div><img class="icon" src="/src/assets/enroll/phone-solid.svg">{{ companyInfo.ctel }}</div>
+                    <div><img class="icon" src="/src/assets/enroll/person-walking-solid.svg">{{ companyInfo.address }}</div>
+                    <div><button @click="enroll">등록하기</button></div>
+                </div>
+            </div>
+        </div>
+    </Teleport>
+</div>
+</template>
+<script setup>
+import { axiosAddress } from '@/stores/axiosAddress';
+import axios from 'axios';
+import { computed, reactive, ref } from 'vue';
+//검색
+const inputCompanyCode =  ref('');
+const inputCompanyCodeCut = computed(()=>{
+    return inputCompanyCode.value.trim();
+})
+const companyInfo = reactive({
+    companyId : '',
+    cname : '',
+    ctel : '',
+    address : '',
+    
+})
+const getInfo = async() => {
+    await axios.get(`${axiosAddress}/getCompanyInfo?code=${inputCompanyCodeCut.value}`,{withCredential : true})
+    .then((res)=>{
+        companyInfo.companyId = res.data.companyId;
+        companyInfo.cname = res.data.cname;
+        companyInfo.ctel = res.data.ctel;
+        companyInfo.address = res.data.address + " " + res.data.detailAddress;
+    })
+    showChangeModal.value = true;
+}
+const enroll = () =>{
+    axios.post(`${axiosAddress}/enroll`,{ companyId : companyInfo.companyId, },{withCredential : true})
+    .then(alert("등록되었습니다."))
+}
+//모달창 여닫기
+const showChangeModal = ref(false);
+const closeModal = () => {
+    showChangeModal.value = false;
+}
+</script>
+<style scoped>
+    .wrap-workplace{
+        display: flex;
+        width: 100%;
+        justify-content: center; /*가로*/
+        align-items: center; /*세로*/
+    }
+    .search-bar-container{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 40%;
+        background-color: #f9f9f9;
+        padding: 10px;
+    }
+    .search-bar{
+        display: flex;
+        width: calc(100% - 10px);
+        justify-content: center;
+        align-items: center;
+        border: 1px solid #4FD1C5;
+    }
+    .input-field{
+        width: 100%;
+        height: 40px;
+        font-size: 22px;
+        text-align: center;
+        border: none;
+    }
+    .search-button{
+        width: 40px;
+        height: 40px;
+        background-color: white;
+    }
+    .modal{
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        padding: 15px;
+        border: 1px, solid, #4FD1C5;
+        background-color: #ffffff;
+        border-radius: 20px;
+        width: 400px;
+        height: 200px;
+
+        display: flex;
+        flex-direction: column; /* 세로 방향으로 정렬 */
+        justify-content: center; /* 세로 축 중앙 정렬 */
+        align-items: center; /* 가로 축 중앙 정렬 */
+        animation: slideUp 0.3s ease;
+    }
+    .modal-overlay {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.5);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        z-index: 999;
+        animation: fadeIn 0.3s ease;
+    }
+    .modal .title {
+    display: flex;
+    align-items: center;
+    font-size: 20px;
+    font-weight: bold;
+    color: #333;
+    margin-bottom: 10px;
+    }
+    .icon{
+        margin-right: 10px;
+        width: 30px;
+        height: 30px;
+    }
+    .content div{
+        margin-top: 10px;
+        margin-bottom: 10px;
+        display: flex;
+        align-items: center;
+    }
+    .content button{
+        width: 100%;
+        border : 0;
+        background-color: #4FD1C5;
+        font-size: 16px;
+        color: #f9f9f9;
+    }
+    .content button:hover{
+        background-color: lightseagreen;
+    }
+    /* 애니메이션 */
+    @keyframes fadeIn {
+        from {
+            background-color: rgba(0, 0, 0, 0);
+        }
+        to {
+            background-color: rgba(0, 0, 0, 0.6);
+        }
+    }
+
+    @keyframes slideUp {
+        from {
+            transform: translate(-50%, 60%);
+            opacity: 0;
+        }
+        to {
+            transform: translate(-50%, -50%);
+            opacity: 1;
+        }
+    }
+</style>
