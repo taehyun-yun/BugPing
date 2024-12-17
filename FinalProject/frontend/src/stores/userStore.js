0b8b838fbbc6b9084f45eded87636b145bec8929
junho <junho9661@gmail.com>
2024-12-17 18:49:09 +0900

    근무자 전용 - 근무지 등록하기, 과거 근무지 조회 - 계약서 연결, 선택된 회사 피니아에 등록, 헤더에 해당 피니아 연결, role은 배열로 피니아에 저장됨

--- FinalProject/frontend/src/stores/userStore.js
+++ FinalProject/frontend/src/stores/userStore.js
@@ -2,7 +2,7 @@ import { defineStore } from "pinia";
 export const useUserStore = defineStore('user',{
     state : () => ({
         userId : '',
-        roles : '',
+        roles : [],
         company : '',
         companies : [],
     }),
